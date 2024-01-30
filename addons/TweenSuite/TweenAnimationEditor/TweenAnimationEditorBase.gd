2c5ea8138ad2fb8e2a0fd6ff6c9058609aee74d7
kobewi <kobewi4e@gmail.com>
2024-01-30 21:18:07 +0100

    Safe play

--- addons/TweenSuite/TweenAnimationEditor/TweenAnimationEditorBase.gd
+++ addons/TweenSuite/TweenAnimationEditor/TweenAnimationEditorBase.gd
@@ -2,6 +2,8 @@
 extends Control
 
 @onready var animation_view: ScrollContainer = $AnimationView
+@onready var play_button: Button = %Play
+@onready var stop_button: Button = %Stop
 
 var animation_editor: Control
 var preview_tween: Tween
@@ -16,8 +18,7 @@ func create_editor(for_animation: TweenAnimation):
 	animation_editor.animation = for_animation
 	animation_view.add_child(animation_editor)
 	
-	%Play.disabled = false
-	%Stop.disabled = false
+	play_button.disabled = false
 
 func new_animation() -> void:
 	create_editor(TweenAnimation.new())
@@ -35,17 +36,43 @@ func play_animation() -> void:
 	animation_editor.push_data()
 	var animation: TweenAnimation = animation_editor.animation
 	
-	create_revert(animation)
+	var root := EditorInterface.get_edited_scene_root()
+	var animation_root := root.get_node(animation.root_path)
+	animation = create_revert(animation, animation_root)
 	
-	preview_tween= create_tween()
-	animation.apply_to_tween(preview_tween, EditorInterface.get_edited_scene_root())
+	preview_tween = create_tween()
+	animation.apply_to_tween(preview_tween, root)
+	preview_tween.finished.connect(apply_revert)
+	
+	stop_button.disabled = false
 
 func stop_animation() -> void:
 	preview_tween.kill()
 	preview_tween = null
+	apply_revert()
 
-func create_revert(from: TweenAnimation):
-	pass
+func create_revert(from: TweenAnimation, animation_root: Node) -> TweenAnimation:
+	var new_anim := TweenAnimation.new()
+	new_anim.root_path = from.root_path
+	var new_steps := []
+	
+	for step: Array in from.steps:
+		new_steps.append(step.filter(func(tweener) -> bool:
+			return tweener is TweenAnimation.PropertyTweenerAnimator))
+		
+		for tweener in new_steps.back():
+			if tweener is TweenAnimation.PropertyTweenerAnimator:
+				var target: Object = tweener.get_target_object(animation_root, tweener.target)
+				revert[[target, tweener.property]] = target.get_indexed(tweener.property)
+	
+	new_anim.steps = new_steps
+	return new_anim
 
 func apply_revert():
-	pass
+	for key: Array in revert:
+		var object: Object = key[0]
+		var property: NodePath = key[1]
+		object.set_indexed(property, revert[key])
+	
+	revert.clear()
+	stop_button.disabled = true
